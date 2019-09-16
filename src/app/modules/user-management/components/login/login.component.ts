639c3db53ef3eb67d6028c91eda178486c5e3be9
Charan <charan.r@pacewisdom.com>
2019-09-16 16:42:04 +0530

    added login page

--- src/app/modules/user-management/components/login/login.component.ts
+++ src/app/modules/user-management/components/login/login.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { FormGroup, FormControl, Validators } from "@angular/forms";
 import { UserManagementService } from "../../services/user-management.service";
-import { Router } from '@angular/router';
+import { Router } from "@angular/router";
 
 @Component({
   selector: "app-login",
@@ -13,28 +13,68 @@ export class LoginComponent implements OnInit {
   hide = true;
 
   loginForm: FormGroup;
+  token: any;
 
-  constructor(private userManagementService: UserManagementService,private router:Router) {}
+  constructor(
+    private userManagementService: UserManagementService,
+    private router: Router
+  ) {}
+  emailPattern = "^[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,4}$";
 
   ngOnInit() {
+    localStorage.clear();
     this.loginForm = new FormGroup({
-      username: new FormControl(null, [Validators.required]),
+      username: new FormControl(null, [
+        Validators.required,
+        Validators.email,
+        Validators.pattern(this.emailPattern)
+      ]),
       password: new FormControl(null, Validators.required)
     });
+  }
+  getErrorMessage() {
+    return this.loginForm.get("username").hasError("required")
+      ? "You must enter a value"
+      : this.loginForm.get("username").hasError("email")
+      ? "Not a valid email"
+      : "";
+  }
+  getErrorMessagepwd() {
+    return this.loginForm.get("password").hasError("required")
+      ? "You must enter a value"
+      : "";
   }
   onSubmit() {
     console.log(this.loginForm);
 
     if (this.loginForm.valid) {
-      this.router.navigate(['users'])
-      // this.userManagementService.submitForm(this.loginForm.value).subscribe(
-      //   res => {
-      //     console.log("success response", res);
-      //   },
-      //   error => {
-      //     console.log("error response", error);
-      //   }
-      // );
+      this.userManagementService.submitForm(this.loginForm.value).subscribe(
+        res => {
+          // console.log(res);
+          // console.log('response', res)
+          // if(res.status == 'success') {
+          //   const { token, role_id, user_id ,name} = res;
+          //   localStorage.setItem("token", token);
+          //   localStorage.setItem("userId", user_id);
+          //   localStorage.setItem("roleId", role_id);
+          //   localStorage.setItem("name", name);
+          //   this.router.navigate(["/users"]);
+          // } else {
+          //   Swal.fire({
+          //     type: 'error',
+          //     title: res.message,
+          //   })
+          // }
+        },
+        error => {
+          //Swal.fire("login fail", error);
+          // Swal.fire({
+          //   type: 'error',
+          //   title: 'Oops...',
+          //   text:error.message,
+          // });
+        }
+      );
     }
   }
 }
